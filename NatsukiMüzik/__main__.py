e1b9598d9d76312f3e26772de2fed5960a72c7b6
KIZILSANCAK AİLESİ <90479077+kizilsancakailesi@users.noreply.github.com>
2021-09-12 14:26:29 +0300

    Update and rename NatsukiMusic/__main__.py to NatsukiMüzik/__main__.py

--- NatsukiMüzik/__main__.py
+++ NatsukiMüzik/__main__.py
@@ -1,4 +1,4 @@
-# NatsukiMusic (Telegram bot project)
+# Ellycarlmusicbot (Telegram bot project)
 # Copyright (C) 2021  Sadew Jayasekara & Rojserbest
 
 # This program is free software: you can redistribute it and/or modify
